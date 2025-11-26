1d7e0294afc665093c7803fef479edd49866e1b4
Gabriel Jancoski <106913336+Midvirus@users.noreply.github.com>
2025-11-25 22:35:43 -0300

    Create RoutineRepository.java

---
+++ backend/src/main/java/com/poo2/bio_factor/repository/RoutineRepository.java
@@ -0,0 +1,9 @@
+package com.poo2.bio_factor.repository;
+
+import com.poo2.bio_factor.model.Routine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
+}
