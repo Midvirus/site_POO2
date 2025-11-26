8f665e38f6d06423d084692d0f80a46cdcc19ce0
Gabriel Jancoski <106913336+Midvirus@users.noreply.github.com>
2025-11-25 22:54:22 -0300

    Create ExerciseController.java

---
+++ backend/src/main/java/com/poo2/bio_factor/controller/ExerciseController.java
@@ -0,0 +1,23 @@
+package com.poo2.bio_factor.controller;
+
+import com.poo2.bio_factor.model.Exercise;
+import com.poo2.bio_factor.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exercises")
+@CrossOrigin(origins = "*")
+public class ExerciseController {
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    // Busca apenas os exercícios que NÃO estão vinculados a treinos (Templates)
+    @GetMapping("/templates")
+    public List<Exercise> getTemplates() {
+        return exerciseRepository.findByWorkoutIsNull();
+    }
+}
